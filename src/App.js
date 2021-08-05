fc8ef670d872c09711c437e14a1e2d6f6a807be7
Alberto Alexis Dominguez Ruiz <t1mk1ll3r4ll@gmail.com>
2021-08-04 19:43:59 -0500

    owo

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React from 'react'
+import {BrowserRouter as Router, Switch, Route, Link, NavLink} from "react-router-dom"
+import Civilization from './components/Civilization'
+import Contacto from './components/Contacto'
+import Inicio from './components/Inicio'
+import Nosotros from './components/Nosotros'
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+    <div className="container">
+      <div className="btn-group">
+        <NavLink to="/" className="btn btn-dark mt-5 " activeClassName="active"> 
+          Inicio
+        </NavLink>
+        <Link to="contacto" className="btn btn-dark mt-5 ">
+          Contacto
+        </Link>
+        <NavLink to="/nosotros" className="btn btn-dark mt-5" activeClassName="active">
+          Nosotros
+        </NavLink>
+      </div>
+      <h1>Navbar</h1>
+      <hr/>
+      <Switch>
+        <Route path="/nosotros/:id">
+          <Civilization></Civilization>
+        </Route>
+        <Route path="/contacto">
+         <Contacto></Contacto>
+        </Route>
+        <Route path="/nosotros">
+          <Nosotros></Nosotros>       
+        </Route>
+        <Route path="/">
+          <Inicio></Inicio>
+        </Route>
+      </Switch>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/Nosotros.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from 'react'
+import {Link} from 'react-router-dom'
+const Nosotros = () => {
+    const [equipo, setEquipo] = useState([])
+    useEffect(()=>{
+obtenerDatos()
+    },[])
+
+    const obtenerDatos = async () =>{
+       const data = await fetch('https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations')
+       const users = await data.json()
+        setEquipo(users.civilizations)
+    }
+    return (
+        <div>
+            <h1> Esta es la pagina de nosotros</h1>
+            <ul>
+                {
+                    equipo.map(item => (
+                        <li key={item.id}>
+                            <Link to={`/nosotros/${item.id}`}>
+                            {item.name} - {item.expansion}
+                            </Link>
+                         </li>
+                    ))
+                }
+            </ul>
+        </div>
+    )
+}
+
+export default Nosotros
